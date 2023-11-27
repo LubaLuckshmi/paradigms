beb3a3ab77337e95fb9c0d55c82f8ef409d625e5
Lubaluckshmi <lubov.loskutova@mail.ru>
2023-11-27 13:02:31 +0300

    Added seminar 2

--- src/main/java/seminar2/Task1.java
+++ src/main/java/seminar2/Task1.java
@@ -1,6 +1,6 @@
 package seminar2;
 
-public class Main {
+public class Task1 {
     public static void main(String[] args) {
         int[][] matrix = {
                 {1, 2, 3},
--- src/main/java/seminar2/Task2.java
+++ src/main/java/seminar2/Task2.java
@@ -1,2 +1,28 @@
-package seminar2;public class Task2 {
-}
+package seminar2;
+
+public class Task2 {
+
+    //Я выбрала для решения данной задачи процедурную
+    // парадигму, так как задача не требует создания
+    // каких-то сложных структур данных или использования
+    // объектно-ориентированного подхода.
+
+
+        public static String convertToBase(int number, int base) {
+            if (base < 2 || base > 36) {
+                return "Invalid base";
+            }
+
+            return Integer.toString(number, base);
+        }
+
+        public static void main(String[] args) {
+            int decimalNumber = 10;
+            int base = 8;
+            String result = convertToBase(decimalNumber, base);
+            System.out.println("Number " + decimalNumber + " in base " + base + " is: " + result);
+        }
+    }
+
+
+
